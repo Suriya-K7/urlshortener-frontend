62d6e6f23dc67392190fa2598f8e10cb1695d9c0
Suriya-K7 <usraising@gmail.com>
2023-07-23 13:29:00 +0530

    url updated

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "https://url-shortener-be.onrender.com/",
 });
 
-// baseURL: "https://url-shortener-be.onrender.com/",
+// baseURL: "http://localhost:3001/",
